c66da7f9625d79aa31d46f84def73d241180f5a2
Trinh Thuy <trinhthuy210100@gmail.com>
2020-09-21 21:55:53 +0700

    delete

--- _dev/trinh_thuy_practices/GlobularVolume.js
+++
@@ -1,7 +0,0 @@
-function CalGlobularVolume() {
-    const pi = 3.1416;
-    var a = Number(prompt("Input Globular's radius: "));
-    var V = 4 / 3 * pi * r * r * r;
-    console.log("Globular's Volume is: "+V);
-}
-
