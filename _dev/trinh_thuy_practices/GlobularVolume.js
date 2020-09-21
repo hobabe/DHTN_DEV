757b2eacc7464a170f9d93b818511c2d88bd9810
Trinh Thuy <trinhthuy210100@gmail.com>
2020-09-21 22:06:42 +0700

    add trinh-thuy-practices folder to trinh-thuy-practices branch

---
+++ _dev/trinh_thuy_practices/GlobularVolume.js
@@ -0,0 +1,7 @@
+function CalGlobularVolume() {
+    const pi = 3.1416;
+    var a = Number(prompt("Input Globular's radius: "));
+    var V = 4 / 3 * pi * r * r * r;
+    console.log("Globular's Volume is: "+V);
+}
+
